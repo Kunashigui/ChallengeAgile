c3b4ee104ad3684cc99c6dbf1d4d91c2e0748132
fjhernandez <felipe.hernandez@encora.com>
2024-12-05 02:18:48 -0600

    Minor fix

--- src/main/java/com/challenge/agileenginechallenge/controller/OrderController.java
+++ src/main/java/com/challenge/agileenginechallenge/controller/OrderController.java
@@ -4,7 +4,6 @@ import com.challenge.agileenginechallenge.dto.OrderDTO;
 import com.challenge.agileenginechallenge.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/challenge/agileenginechallenge/service/OrderItemService.java
+++ src/main/java/com/challenge/agileenginechallenge/service/OrderItemService.java
@@ -9,9 +9,9 @@ import java.util.UUID;
 @Service
 public interface OrderItemService {
 
-    public List<OrderItemDTO> getOrderItems();
-    public OrderItemDTO getOrderItemById(UUID id);
-    public OrderItemDTO createOrderItem(OrderItemDTO orderItem);
-    public OrderItemDTO updateOrderItem(UUID id, OrderItemDTO orderItem);
-    public void deleteOrderItem(UUID id);
+    List<OrderItemDTO> getOrderItems();
+    OrderItemDTO getOrderItemById(UUID id);
+    OrderItemDTO createOrderItem(OrderItemDTO orderItem);
+    OrderItemDTO updateOrderItem(UUID id, OrderItemDTO orderItem);
+    void deleteOrderItem(UUID id);
 }
